7f152664da6bc72a78aeed1949b5faede8876d16
Saad Nahas <167629574+SaaDN1@users.noreply.github.com>
2024-10-01 19:52:16 +0300

    Update Main.java

--- src/main/java/cpit252/fcit/kau/edu/sa/Main.java
+++ src/main/java/cpit252/fcit/kau/edu/sa/Main.java
@@ -2,6 +2,6 @@ package cpit252.fcit.kau.edu.sa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Saad!");
     }
 }
