e3af4c7f1882d4870fc57a6868ca1205b92b0834
Khalid Alharbi <kalharbi@users.noreply.github.com>
2024-10-02 13:33:40 +0300

    fixed car search

--- src/main/java/cpit252/fcit/kau/edu/sa/Main.java
+++ src/main/java/cpit252/fcit/kau/edu/sa/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Car Fixer!");
         System.out.println("Added a new feature");
+        System.out.println("Fixed car Search");
     }
 }
